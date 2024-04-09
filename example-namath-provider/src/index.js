d81d5c51adb33ae805f95705f48ac820364bd585
Joshua May <joshua.p.may@gmail.com>
2024-04-09 13:31:53 -0400

    Update index.js

--- example-namath-provider/src/index.js
+++ example-namath-provider/src/index.js
@@ -253,8 +253,7 @@ class MyMessageAPI extends EventEmitter {
 module.exports = {
 
   jancy_props: {
-    registryVersion: 1,
-    canDisable: false
+    registryVersion: 1
   },
 
   /* --------------------------------------------------------------------------
